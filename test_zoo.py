cb77349f8511b2b31a8333fcaf537f17c9d0d574
PlyJB <ploy.j0912@gmail.com>
2024-11-11 05:31:50 +0000

    pen rai nor

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,35 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
-
+    def test_UnderZero_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-3), "Invalid Input")
+    def test_ticket_price_class2(self):
+        self.assertEqual(self.zoo.get_ticket_price(10), 50)
+    def test_ticket_price_class3(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    def test_ticket_price_class4(self):
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+    def test_ticket_price_class5(self):
+        self.assertEqual(self.zoo.get_ticket_price(100), 100)
+    
+    def test_Bound1_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid Input")
+    def test_Bound2_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    def test_Bound3_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    def test_Bound4_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    def test_Bound5_ticket_price_(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)    
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: #age < 0
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: #age < 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: #age < 21
             return 150
         elif age >= 60:
             return 100
+        elif age < 0 : #to handle input Under 0
+            return "Invalid Input"
